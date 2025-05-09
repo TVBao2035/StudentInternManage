df41bf751ad618cb44c52236c8a5e78b3df7c5e3
PC-BAOTRUONG\bao.truong <truongvanbao126@gmail.com>
2025-05-09 09:50:53 +0700

    add id proprety in loginResponse

--- back-end/back-end/DTO/Auth/LoginResponse.cs
+++ back-end/back-end/DTO/Auth/LoginResponse.cs
@@ -3,6 +3,7 @@ namespace back_end.DTO.Auth
 {
     public class LoginResponse
     {
+        public Guid Id { get; set; }
         public string Email { get; set; }
         public string Name { get; set; }
         public List<string> Roles { get; set; }
